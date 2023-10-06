a8cefa85a1d62fe6e1cc408569fec8cd2eb66c90
daye-oa <dayeomona@gmail.com>
2023-10-05 18:17:53 -0700

    leap

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,7 @@ int main(void)
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	printf("Size of double: %zu byte(s)\n", sizeof(double));
-	printf("Size of long double: %zu byte(s)\n", sizeof(long double));\return 0;
+	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+	return 0;
 }
 
